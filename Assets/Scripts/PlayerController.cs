62b788ca86fbad7bdb25c633853de80d4a33ce4b
RemiRaff <remi_raffaghello@hotmail.com>
2023-01-03 11:19:45 +0100

    player input added with Send Messages

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    // speed of farmer
+    public float speed = 2000.0f;
+
+    private Rigidbody _rb;
+    private float movementX;
+    private float movementY;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Debug.Log("Move");
+        _rb.velocity = new Vector2(movementValue.Get<Vector2>().x, 0) * speed * Time.deltaTime;
+    }
+
+    void OnFire()
+    {
+        Debug.Log("Fire");
     }
 }
